36e11e2e0463614e1bdaf681453fabe819499327
hrshtt <harshsha64@gmail.com>
2025-07-14 15:44:44 +0530

    fixed burner miner drill giving no_op

--- replay-lab/action-based-logs/script/logistics.lua
+++ replay-lab/action-based-logs/script/logistics.lua
@@ -99,12 +99,18 @@ function logistics.find_primary_inventory_index(entity)
     return defines.inventory.lab_input
   elseif entity.type == "turret" or entity.type == "ammo-turret" then
     return defines.inventory.turret_ammo
+  elseif entity.type == "artillery-turret" then
+    return defines.inventory.artillery_turret_ammo
   elseif entity.type == "roboport" then
     return defines.inventory.roboport_robot
+  elseif entity.type == "beacon" then
+    return defines.inventory.beacon_modules
   elseif entity.type == "character" then
     return defines.inventory.character_main
-  elseif entity.type == "car" or entity.type == "tank" or entity.type == "spider-vehicle" then
+  elseif entity.type == "car" or entity.type == "tank" then
     return defines.inventory.car_trunk
+  elseif entity.type == "spider-vehicle" then
+    return defines.inventory.spider_trunk
   elseif entity.type == "cargo-wagon" then
     return defines.inventory.cargo_wagon
   elseif entity.type == "artillery-wagon" then
@@ -127,15 +133,23 @@ function logistics.get_all_relevant_inventories(entity)
   local inventories = {}
   
   if entity.type == "furnace" then
-    -- For furnaces, track both fuel and result slots since players interact with both
-    inventories[defines.inventory.furnace_source] = "fuel"
+    -- For furnaces, track fuel, result, and modules slots since players interact with all
+    inventories[defines.inventory.furnace_source] = "source"
     inventories[defines.inventory.furnace_result] = "result"
+    if entity.get_inventory(defines.inventory.furnace_modules) then
+      inventories[defines.inventory.furnace_modules] = "modules"
+    end
   elseif entity.type == "mining-drill" then
-    -- For mining drills, track both output and modules
+    -- For mining drills, track modules and fuel (for burner drills)
     if entity.get_inventory(defines.inventory.mining_drill_modules) then
       inventories[defines.inventory.mining_drill_modules] = "modules"
     end
-    -- Note: mining drills don't have a regular output inventory, they output directly to belts/inserters
+    -- Burner mining drills have a fuel inventory
+    if entity.get_inventory(defines.inventory.fuel) then
+      inventories[defines.inventory.fuel] = "fuel"
+    end
+    -- Note: Mining drills don't have an output inventory accessible via API
+    -- They output directly to belts/inserters, which is why extractions appear as no-ops
   elseif entity.type == "assembling-machine" or entity.type == "oil-refinery" or entity.type == "chemical-plant" then
     inventories[defines.inventory.assembling_machine_input] = "input"
     inventories[defines.inventory.assembling_machine_output] = "output"
@@ -144,9 +158,69 @@ function logistics.get_all_relevant_inventories(entity)
     inventories[defines.inventory.rocket_silo_input] = "input"
     inventories[defines.inventory.rocket_silo_output] = "output"
     inventories[defines.inventory.rocket_silo_modules] = "modules"
+    if entity.get_inventory(defines.inventory.rocket_silo_result) then
+      inventories[defines.inventory.rocket_silo_result] = "result"
+    end
+    if entity.get_inventory(defines.inventory.rocket_silo_rocket) then
+      inventories[defines.inventory.rocket_silo_rocket] = "rocket"
+    end
   elseif entity.type == "lab" then
     inventories[defines.inventory.lab_input] = "input"
     inventories[defines.inventory.lab_modules] = "modules"
+  elseif entity.type == "beacon" then
+    -- For beacons, track modules
+    if entity.get_inventory(defines.inventory.beacon_modules) then
+      inventories[defines.inventory.beacon_modules] = "modules"
+    end
+  elseif entity.type == "roboport" then
+    -- For roboports, track both robots and repair materials
+    if entity.get_inventory(defines.inventory.roboport_robot) then
+      inventories[defines.inventory.roboport_robot] = "robots"
+    end
+    if entity.get_inventory(defines.inventory.roboport_material) then
+      inventories[defines.inventory.roboport_material] = "materials"
+    end
+  elseif entity.type == "car" or entity.type == "tank" then
+    -- For cars/tanks, track trunk and ammo
+    if entity.get_inventory(defines.inventory.car_trunk) then
+      inventories[defines.inventory.car_trunk] = "trunk"
+    end
+    if entity.get_inventory(defines.inventory.car_ammo) then
+      inventories[defines.inventory.car_ammo] = "ammo"
+    end
+  elseif entity.type == "spider-vehicle" then
+    -- For spider vehicles, track trunk, ammo, and trash
+    if entity.get_inventory(defines.inventory.spider_trunk) then
+      inventories[defines.inventory.spider_trunk] = "trunk"
+    end
+    if entity.get_inventory(defines.inventory.spider_ammo) then
+      inventories[defines.inventory.spider_ammo] = "ammo"
+    end
+    if entity.get_inventory(defines.inventory.spider_trash) then
+      inventories[defines.inventory.spider_trash] = "trash"
+    end
+  elseif entity.type == "artillery-turret" then
+    -- For artillery turrets, use the specific artillery ammo inventory
+    if entity.get_inventory(defines.inventory.artillery_turret_ammo) then
+      inventories[defines.inventory.artillery_turret_ammo] = "ammo"
+    end
+  elseif entity.type == "character" then
+    -- For characters, track all relevant inventories
+    if entity.get_inventory(defines.inventory.character_main) then
+      inventories[defines.inventory.character_main] = "main"
+    end
+    if entity.get_inventory(defines.inventory.character_guns) then
+      inventories[defines.inventory.character_guns] = "guns"
+    end
+    if entity.get_inventory(defines.inventory.character_ammo) then
+      inventories[defines.inventory.character_ammo] = "ammo"
+    end
+    if entity.get_inventory(defines.inventory.character_armor) then
+      inventories[defines.inventory.character_armor] = "armor"
+    end
+    if entity.get_inventory(defines.inventory.character_trash) then
+      inventories[defines.inventory.character_trash] = "trash"
+    end
   else
     -- For simple entities, just use the primary inventory
     local primary_idx = logistics.find_primary_inventory_index(entity)
@@ -283,6 +357,7 @@ logistics.player_accessible_types = {
   ["artillery-turret"] = true,
   ["lab"] = true,
   ["roboport"] = true,
+  ["beacon"] = true,
   ["reactor"] = true,
   ["boiler"] = true,
   ["inserter"] = true,
